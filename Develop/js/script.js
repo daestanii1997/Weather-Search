0e6316c9924134fa06429b8492fe847be64bd898
Dae <daestanii1997@gmail.com>
2023-08-12 13:55:04 -0600

    formatted header and search area, am able to access input value for API

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,32 +1,42 @@
 // var apiKey: 'be8e0248d5c480564f7b163365324150'
 
 var searchBtn = document.querySelector('#searchBtn');
-var cityInput = document.querySelector('.city-input');
+
+// API call function
 
 function getApi() {
 
     var weatherUrl = 'http://api.openweathermap.org/geo/1.0/reverse?lat=40.6111&lon=-111.8999&limit=5&appid=be8e0248d5c480564f7b163365324150';
 
-fetch(weatherUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log('Weather: ', data);
-    });
+    fetch(weatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log('Weather: ', data);
+        });
 
-var geoCodeUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=be8e0248d5c480564f7b163365324150';
+    var cityInput = document.querySelector('.city-input').value;
+    console.log(cityInput)
 
-fetch(geoCodeUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log('GeoCode: ', data);
-    })
-    .catch(function (error) {
-        console.log('error fetching data', error)
-    })
+    var geoCodeUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + { cityInput } + '&limit=1&appid=be8e0248d5c480564f7b163365324150';
+
+    fetch(geoCodeUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log('GeoCode: ', data);
+        })
+        .catch(function (error) {
+            console.log('error fetching data', error)
+        })
+
+        
 };
 
-   searchBtn.addEventListener('click',getApi);
+searchBtn.addEventListener('click', getApi);
+
+// function searchHistory() {
+//     console.log(cityInput)
+// }
