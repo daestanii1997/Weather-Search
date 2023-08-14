5da265c95342789d203d1f5e0a3b27f7217d3e10
Dae <daestanii1997@gmail.com>
2023-08-14 13:18:33 -0600

    got the apis bothing working and communicating!

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,4 +1,4 @@
-// var apiKey: 'be8e0248d5c480564f7b163365324150'
+//apiKey = 'be8e0248d5c480564f7b163365324150'
 
 var searchBtn = document.querySelector('#searchBtn');
 var asideSection = document.querySelector('#aside-section');
@@ -7,43 +7,58 @@ var asideSection = document.querySelector('#aside-section');
 
 function getApi() {
 
-    var weatherUrl = 'http://api.openweathermap.org/geo/1.0/reverse?lat=40.6111&lon=-111.8999&limit=5&appid=be8e0248d5c480564f7b163365324150';
-
-    fetch(weatherUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log('Weather: ', data);
-        });
-
-    var cityInput = document.querySelector('.city-input').value;
+    let cityInput = document.querySelector('.city-input').value;
     console.log(cityInput)
 
-    var geoCodeUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + { cityInput } + '&limit=1&appid=be8e0248d5c480564f7b163365324150';
+    // Geocode Url to get lat and long
+
+    var geoCodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + '&limit=1&appid=be8e0248d5c480564f7b163365324150';
 
     fetch(geoCodeUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log('GeoCode: ', data);
+            console.log('GeoCode Lat: ', data);
+
+            // weather URL to apply lat and long to get results
+
+            var weatherUrl = 'http://api.openweathermap.org/geo/1.0/reverse?lat=' + data[0].lat + '&lon=' + data[0].lon + '&limit=5&appid=be8e0248d5c480564f7b163365324150';
+
+            fetch(weatherUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log('Weather: ', data);
+                });
         })
         .catch(function (error) {
             console.log('error fetching data', error)
         })
 
-        var historyBtn = document.createElement('button');
-        historyBtn.classList.add('history-btn');
-        historyBtn.textContent = cityInput;
-        asideSection.appendChild(historyBtn);
 
-        localStorage.setItem(historyBtn, cityInput);
-        historyBtn.value = localStorage.getItem('cityInput');
+
+    if (!cityInput) {
+        window.alert('Please enter a city name')
+        // find a way to 
+    }
+
+    searchHistory();
 };
 
 searchBtn.addEventListener('click', getApi);
 
-// function searchHistory() {
-//     console.log(cityInput)
-// }
+// Create separate function for history btn and call it in this function
+// Figure out how to search button label in that function
+
+function searchHistory() {
+    let cityInput = document.querySelector('.city-input').value;
+    let historyBtn = document.createElement('button');
+    historyBtn.classList.add('history-btn');
+    historyBtn.textContent = cityInput;
+    asideSection.appendChild(historyBtn);
+
+    localStorage.setItem(historyBtn, cityInput);
+    historyBtn.value = localStorage.getItem('cityInput');
+};
