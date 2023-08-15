4975739689ed7c40b6b412964ef3a8cd59e73740
Dae <daestanii1997@gmail.com>
2023-08-14 22:40:21 -0600

    Display and format weather information and worked on local storage functionality

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -2,13 +2,20 @@
 
 var searchBtn = document.querySelector('#searchBtn');
 var asideSection = document.querySelector('#aside-section');
-
+var tempDisplay = document.querySelector('.tempDisplay');
+var description = document.querySelector('.description');
+var cityDisplay = document.querySelector('.cityName');
+var dateContainer = document.querySelector('.dateContainer');
+var iconDisplay = document.querySelector('.icon');
+var humidity = document.querySelector('.humidity');
+var windSpeed = document.querySelector('.windSpeed')
 // API call function
 
 function getApi() {
 
     let cityInput = document.querySelector('.city-input').value;
     console.log(cityInput)
+    localStorage.setItem('historyBtn', cityInput);
 
     // Geocode Url to get lat and long
 
@@ -23,28 +30,38 @@ function getApi() {
 
             // weather URL to apply lat and long to get results
 
-            var weatherUrl = 'http://api.openweathermap.org/geo/1.0/reverse?lat=' + data[0].lat + '&lon=' + data[0].lon + '&limit=5&appid=be8e0248d5c480564f7b163365324150';
+            console.log(data[0].lat)
+            console.log(data[0].lon)
+
+            var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&limit=5&appid=be8e0248d5c480564f7b163365324150';
 
             fetch(weatherUrl)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log('Weather: ', data);
+                    console.log('weather:', data);
+
+                    tempDisplay.textContent = data.main.temp + ' °F';
+                    description.textContent = data.weather[0].description;
+                    cityDisplay.textContent = data.name;
+                    // dateContainer.textContent = dayjs().format(MMM,D);
+                    iconDisplay.textContent = data.weather[0].icon;
+                    humidity.textContent = "Humidity: " + data.main.humidity;
+                    windSpeed.textContent = "Wind Speed: " + data.wind.speed;
                 });
         })
         .catch(function (error) {
             console.log('error fetching data', error)
         })
 
-
-
     if (!cityInput) {
         window.alert('Please enter a city name')
-        // find a way to 
+    } else {
+        searchHistory();
+        // tempDisplay.innerHTML = data.weather[0].main.temp;
+        // description.innerHTML = weather.weather[0].description;
     }
-
-    searchHistory();
 };
 
 searchBtn.addEventListener('click', getApi);
@@ -53,12 +70,12 @@ searchBtn.addEventListener('click', getApi);
 // Figure out how to search button label in that function
 
 function searchHistory() {
-    let cityInput = document.querySelector('.city-input').value;
-    let historyBtn = document.createElement('button');
-    historyBtn.classList.add('history-btn');
-    historyBtn.textContent = cityInput;
-    asideSection.appendChild(historyBtn);
 
-    localStorage.setItem(historyBtn, cityInput);
-    historyBtn.value = localStorage.getItem('cityInput');
+    let historyBtn = document.createElement('button');
+    console.log(localStorage.getItem('historyBtn'))
+    historyBtn.classList.add('history-btn');
+    historyBtn.textContent = localStorage.getItem('historyBtn');
+
+    asideSection.appendChild(historyBtn);
 };
+
