380e445c3df6654075b868efc41e9c6f461d44e6
Dae <daestanii1997@gmail.com>
2023-08-21 21:49:52 -0600

    finished 5 day forcast and styling

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,4 +1,4 @@
-//apiKey = 'be8e0248d5c480564f7b163365324150'
+const apiKey = 'be8e0248d5c480564f7b163365324150';
 
 var searchBtn = document.querySelector('#searchBtn');
 var asideSection = document.querySelector('#aside-section');
@@ -9,18 +9,18 @@ var dateContainer = document.querySelector('.dateContainer');
 var iconDisplay = document.querySelector('.icon');
 var humidity = document.querySelector('.humidity');
 var windSpeed = document.querySelector('.windSpeed')
-let cityInput = document.querySelector('.city-input').value;
+var fiveDayContainer = document.querySelector('#fiveDayForcast')
+
 // API call function
 
 function getApi() {
-
     let cityInput = document.querySelector('.city-input').value;
 
     localStorage.setItem('historyBtn', cityInput);
 
     // Geocode Url to get lat and long
 
-    var geoCodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + '&limit=5&appid=be8e0248d5c480564f7b163365324150';
+    var geoCodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + '&limit=5&appid=' + apiKey;
 
     fetch(geoCodeUrl)
         .then(function (response) {
@@ -31,7 +31,7 @@ function getApi() {
 
             // weather URL to apply lat and long to get results
 
-            var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&limit=5&appid=be8e0248d5c480564f7b163365324150';
+            var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&limit=5&appid=' + apiKey;
 
             fetch(weatherUrl)
                 .then(function (response) {
@@ -47,22 +47,62 @@ function getApi() {
                     cityDisplay.textContent = data.name;
                     dateContainer.textContent = dayjs().format('MMM,D');
                     iconDisplay.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
-                    console.log(data.weather[0].icon);
                     humidity.textContent = "Humidity: " + data.main.humidity;
                     windSpeed.textContent = "Wind Speed: " + data.wind.speed;
                 });
+
+            var forcastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + apiKey;
+
+            fetch(forcastUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log('FORCAST ', data);
+
+                    // Day One                
+                    // Date
+                    for(var i = 0; i <= 40 ; i+=8){
+
+                        const dayContainer = document.createElement('div');
+                        dayContainer.setAttribute('class', 'dayContainer');
+                    
+                    const date = document.createElement('p');
+                    date.setAttribute('class', 'fiveDayFormat')
+                    date.textContent = dayjs(data.list[i].dt_txt);
+                    console.log(dayjs(data.list[i].dt_txt))
+                    // Temp
+                    var Tempurature = document.createElement('h3');
+                    Tempurature.setAttribute('class', 'fiveDayFormat')
+                    Tempurature.textContent = data.list[i].main.temp + ' °F';
+                    // Wind
+                    var wind = document.createElement('p');
+                    wind.setAttribute('class', 'fiveDayFormat')
+                    wind.textContent = 'Wind Speed: ' + data.list[i].wind.speed;
+                    // Humidity
+                    var hum = document.createElement('p');
+                    hum.setAttribute('class', 'fiveDayFormat')
+                    hum.textContent = 'Humidity: ' + data.list[i].main.humidity;
+                    // Icon
+                    var Icon = document.createElement('img');
+                    Icon.setAttribute('class', 'fiveDayFormat')
+                    Icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png");
+
+                    fiveDayContainer.appendChild(dayContainer);
+                    dayContainer.appendChild(date);
+                    dayContainer.appendChild(Icon)
+                    dayContainer.appendChild(Tempurature);
+                    dayContainer.appendChild(hum);
+                    dayContainer.appendChild(wind);
+                    
+                    };
         })
         .catch(function (error) {
             console.log('error fetching data', error)
         })
-
-    if (!cityInput) {
-        window.alert('Please enter a city name')
-    } else {
         searchHistory();
-    }
-};
-
+});
+}
 searchBtn.addEventListener('click', getApi);
 
 // Create separate function for history btn and call it in this function
@@ -82,19 +122,3 @@ function searchHistory() {
     }
 
 };
-
-var forcastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ cityInput +'&appid=be8e0248d5c480564f7b163365324150&cnt=5';
-
-fetch(forcastUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log("FORCAST", data);
-        console.log(dayjs(data[0].dt).format(dddd));
-
-    });
-// .catch(function (error) {
-// console.log('error fetching data', error)
-// })
-
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Weather-Search
+
+## Description
+
+I wanted to create a weather application that is simple to use and shows the current weather as well as the 5 day forcast.
+
+## Usage
+
+'''md
+![alt text](Develop/assets/images/Capture.PNG)
+'''
+
+## Link
+
+https://github.com/daestanii1997/Weather-Search
